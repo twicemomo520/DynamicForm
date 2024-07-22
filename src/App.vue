7e38eea6b7c68f30a14dd5b619d5f7ee0bf75c4a
twicemomo520 <tte09567@gmail.com>
2024-07-22 09:41:14 +0800

    init vue version 1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <div class="wrapper">
+    <HelloWorld msg="You did it!" />
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
