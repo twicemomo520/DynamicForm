de53cb32f3d1a99fd6f80789b4eb05034218b367
twicemomo520 <tte09567@gmail.com>
2024-08-17 19:34:43 +0800

    state Filter ok 2

--- src/views/ManageSurvey.vue
+++ src/views/ManageSurvey.vue
@@ -154,29 +154,52 @@ export default{
       stateFilterChange(state){
         
         if (state == '未開放'){
-            this.currentStateFilter = "未開放";
-            let newSubData = this.data.pages.filter(item => {
-                return this.openOrClose(item.firstPage.startDate, item.firstPage.endDate) == "未開放";   
-            });
-            this.subData = newSubData.slice(0,10);
+            if (this.currentStateFilter != "未開放"){
+                this.currentStateFilter = "未開放"
+                let newSubData = this.data.pages.filter(item => {
+                    return this.openOrClose(item.firstPage.startDate, item.firstPage.endDate) == "未開放";   
+                })
+                this.subData = newSubData.slice(0,10);
+            }
+            else{
+                this.currentStateFilter = ""
+                this.subData = this.data.pages.slice(0,10); 
+            }
+            
         }
-
         if (state == '進行中'){
-            this.currentStateFilter = "進行中";
-            let newSubData = this.data.pages.filter(item => {
-                return this.openOrClose(item.firstPage.startDate, item.firstPage.endDate) == "進行中";   
-            });
-            this.subData = newSubData.slice(0,10);
+            if (this.currentStateFilter != "進行中"){
+                this.currentStateFilter = "進行中"
+                let newSubData = this.data.pages.filter(item => {
+                    return this.openOrClose(item.firstPage.startDate, item.firstPage.endDate) == "進行中";   
+                })
+                this.subData = newSubData.slice(0,10);
+            }
+            else{
+                this.currentStateFilter = ""
+                this.subData = this.data.pages.slice(0,10); 
+            }
+            
         }
-
         if (state == '結束'){
-            this.currentStateFilter = "結束";
-            let newSubData = this.data.pages.filter(item => {
-                return this.openOrClose(item.firstPage.startDate, item.firstPage.endDate) == "結束";   
-            });
-            this.subData = newSubData.slice(0,10);
+            if (this.currentStateFilter != "結束"){
+                this.currentStateFilter = "結束"
+                let newSubData = this.data.pages.filter(item => {
+                    return this.openOrClose(item.firstPage.startDate, item.firstPage.endDate) == "結束";   
+                })
+                this.subData = newSubData.slice(0,10);
+            }
+            else{
+                this.currentStateFilter = ""
+                this.subData = this.data.pages.slice(0,10); 
+            }
+            
         }
       },
+    //   sortItem(event){
+    //     let value = event.target.value
+    //     if (value == 'idS')
+    //   },
     
     },
     computed:{
@@ -220,10 +243,14 @@ export default{
                     <p @click="stateFilterChange('結束')" :class="{'after':currentStateFilter === '結束'}">結束</p>
                 </div>    
                 <div class="selectArea">
-                    <select name="" id="">
-                        <option value="id">依照編號</option>
-                        <option value="dateAsc">依照日期 近到遠</option>
-                        <option value="dateDesc">依照日期 遠到近</option>
+                    <span>排序</span>
+                    <select name="" id="" >
+                        <option value="idAsc">編號( 最小 )</option>
+                        <option value="idDesc">編號( 最大 )</option>
+                        <option value="dateStartAsc">開始日期( 最近 )</option>
+                        <option value="dateStartDesc">開始日期( 最遠 )</option>
+                        <option value="dateEndAsc">結束日期( 最近 )</option>
+                        <option value="dateEndDesc">結束日期( 最遠 )</option>
                     </select>
                 </div>
             </div>
@@ -412,7 +439,7 @@ $baby-blue: #f8faff;
             display: flex;
             align-items: center;
             justify-content: space-between;
-            background-color: #f0ddcbc6;
+            background-color: #f0ddcb;
             padding: 0 10px;
             
             .content{
@@ -565,112 +592,9 @@ $baby-blue: #f8faff;
                     }
 
                     tr.hovered {
-                        transform: scale(1.02);
                         transition: all 0.2s ease-in;
                         box-shadow: 2px 2px 12px rgba(0,0,0,0.2), -1px -1px 8px rgba(0,0,0,0.2);
-
                     }
-                    // .bubbly-button{
-                    //     font-family: 'Helvetica', 'Arial', sans-serif;
-                    //     display: inline-block;
-                    //     font-size: 14px;
-                    //     padding: 1em 2em;
-                    //     background-color: $button-bg;
-                    //     color: $button-text-color;
-                    //     border: none;
-                    //     border-radius: 12px;
-                    //     cursor: pointer;
-                    //     position: relative;
-                    //     transition: transform ease-in 0.1s, box-shadow ease-in 0.25s;
-                    //     box-shadow: 0 2px 25px rgba(255, 0, 130, 0.5);
-                        
-                    //     &:focus {
-                    //         outline: 0;
-                    //     }
-                        
-                    //     &:before, &:after{
-                    //         position: absolute;
-                    //         content: '';
-                    //         display: block;
-                    //         width: 140%;
-                    //         height: 100%;
-                    //         left: -20%;
-                    //         z-index: -1000;
-                    //         transition: all ease-in-out 0.5s;
-                    //         background-repeat: no-repeat;
-                    //     }
-                        
-                    //     &:before{
-                    //         display: none;
-                    //         top: -75%;
-                    //         background-image:  
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%),
-                    //         radial-gradient(circle,  transparent 20%, $button-bg 20%, transparent 30%),
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%), 
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%),
-                    //         radial-gradient(circle,  transparent 10%, $button-bg 15%, transparent 20%),
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%),
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%),
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%),
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%);
-                    //     background-size: 10% 10%, 20% 20%, 15% 15%, 20% 20%, 18% 18%, 10% 10%, 15% 15%, 10% 10%, 18% 18%;
-                    //     //background-position: 0% 80%, -5% 20%, 10% 40%, 20% 0%, 30% 30%, 22% 50%, 50% 50%, 65% 20%, 85% 30%;
-                    //     }
-                        
-                    //     &:after{
-                    //         display: none;
-                    //         bottom: -75%;
-                    //         background-image:  
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%), 
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%),
-                    //         radial-gradient(circle,  transparent 10%, $button-bg 15%, transparent 20%),
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%),
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%),
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%),
-                    //         radial-gradient(circle, $button-bg 20%, transparent 20%);
-                    //     background-size: 15% 15%, 20% 20%, 18% 18%, 20% 20%, 15% 15%, 10% 10%, 20% 20%;
-                    //     //background-position: 5% 90%, 10% 90%, 10% 90%, 15% 90%, 25% 90%, 25% 90%, 40% 90%, 55% 90%, 70% 90%;
-                    //     }
-                        
-                    //     &:active{
-                    //         transform: scale(0.9);
-                    //         background-color: darken($button-bg, 5%);
-                    //         box-shadow: 0 2px 25px rgba(255, 0, 130, 0.2);
-                    //     }
-                        
-                    //     &.animate{
-                    //         &:before{
-                    //         display: block;
-                    //         animation: topBubbles ease-in-out 0.75s forwards;
-                    //         }
-                    //         &:after{
-                    //         display: block;
-                    //         animation: bottomBubbles ease-in-out 0.75s forwards;
-                    //         }
-                    //     }
-                    //     }
-                    //     @keyframes topBubbles {
-                    //     0%{
-                    //         background-position: 5% 90%, 10% 90%, 10% 90%, 15% 90%, 25% 90%, 25% 90%, 40% 90%, 55% 90%, 70% 90%;
-                    //     }
-                    //         50% {
-                    //         background-position: 0% 80%, 0% 20%, 10% 40%, 20% 0%, 30% 30%, 22% 50%, 50% 50%, 65% 20%, 90% 30%;}
-                    //     100% {
-                    //         background-position: 0% 70%, 0% 10%, 10% 30%, 20% -10%, 30% 20%, 22% 40%, 50% 40%, 65% 10%, 90% 20%;
-                    //     background-size: 0% 0%, 0% 0%,  0% 0%,  0% 0%,  0% 0%,  0% 0%;
-                    //     }
-                    //     }
-                    //     @keyframes bottomBubbles {
-                    //     0%{
-                    //         background-position: 10% -10%, 30% 10%, 55% -10%, 70% -10%, 85% -10%, 70% -10%, 70% 0%;
-                    //     }
-                    //     50% {
-                    //         background-position: 0% 80%, 20% 80%, 45% 60%, 60% 100%, 75% 70%, 95% 60%, 105% 0%;}
-                    //     100% {
-                    //         background-position: 0% 90%, 20% 90%, 45% 70%, 60% 110%, 75% 80%, 95% 70%, 110% 10%;
-                    //     background-size: 0% 0%, 0% 0%,  0% 0%,  0% 0%,  0% 0%,  0% 0%;
-                    //     }    
-                    // }
                     
                 }
             }
