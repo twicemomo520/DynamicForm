b4fc4b4e00144bb37fdcc361f1d0c3d58af5459d
twicemomo520 <tte09567@gmail.com>
2024-10-22 13:45:12 +0800

    commit

--- src/stores/ngrok.js
+++ src/stores/ngrok.js
@@ -1,11 +1,8 @@
-﻿import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+﻿import { defineStore } from 'pinia'
 
 export const useNgrokStore = defineStore('ngrok', {
-  state(){
-    return{
-        ngrokPath: "https://4189-2001-b011-c006-3a52-1c21-d10a-a2d3-8aca.ngrok-free.app",
-        localhost: "http://localhost"
-    }
-  }
+  state: () => ({
+    ngrokPath: "https://5a7c-61-222-241-251.ngrok-free.app",
+    localhost: "http://localhost"
+  }),
 })
