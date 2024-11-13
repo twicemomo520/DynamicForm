e3a21b3bee6a133c77dcc9d94cdd3e8882d67f5c
twicemomo520 <tte09567@gmail.com>
2024-11-13 13:36:02 +0800

    comit

--- src/stores/ngrok.js
+++ src/stores/ngrok.js
@@ -2,7 +2,7 @@
 
 export const useNgrokStore = defineStore('ngrok', {
   state: () => ({
-    ngrokPath: "https://05d9-61-64-13-28.ngrok-free.app",
+    ngrokPath: " https://ac49-2001-b011-c006-d1dd-f846-ea9c-1392-4ae9.ngrok-free.app",
     localhost: "http://localhost:8081"
   }),
 })
